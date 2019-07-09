975cbe825bbb38a1123942188a5d88a0cf0b33d2
秦财武 <2641837080@qq.com>
2019-07-09 14:17:14 +0800

    [秦财武]修改输出内容

--- src/main/java/com/briup/apps/smss/web/controller/Qincw1.java
+++ src/main/java/com/briup/apps/smss/web/controller/Qincw1.java
@@ -3,6 +3,9 @@ package com.briup.apps.smss.web.controller;
 public class Qincw1 {
   public void main (String[] args)
   {
-	  System.out.println("hello word");
+	  System.out.println("冯虎虎");
+	  System.out.println(" 邓小涛我爱你");
+	  System.out.println("草八卦你真丑");
+	  System.out.println("郭瓜皮真自恋");
   }
 }
