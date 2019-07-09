4d4f56fde541c953740de3e84245e0afe88b3816
秦财武 <2641837080@qq.com>
2019-07-09 14:21:54 +0800

    [秦财武] 输出一群瓜皮

--- src/main/java/com/briup/apps/smss/web/controller/Qincw.java
+++ src/main/java/com/briup/apps/smss/web/controller/Qincw.java
@@ -1,5 +1,6 @@
 package com.briup.apps.smss.web.controller;
 
 public class Qincw { 
-
+	System.out.println("hello word");
+	System.out.println("hello word");
 }
---
+++ src/main/resources/Qincw2.java
@@ -0,0 +1,13 @@
+
+public class Qincw2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("冯虎虎");
+		System.out.println("草八卦");
+		System.out.println("小涛涛");
+		System.out.println("郭");
+		System.out.println("hello word");
+	}
+
+}
